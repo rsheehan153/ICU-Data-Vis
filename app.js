17f589459579977f2e589692a9cc33bfa5da8e73
Ryan Sheehan <ryan.sheehan@plymouth.ac.uk>
2020-06-07 13:25:07 +0100

    Updated for Demo
    Signed-off-by: Ryan Sheehan <ryan.sheehan@plymouth.ac.uk>

--- app.js
+++ app.js
@@ -17,7 +17,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/public'));
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
